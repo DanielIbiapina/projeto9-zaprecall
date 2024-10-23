53045b4a3f7b8c70e9a6deca62a28c95bd0db8ad
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2024-10-23 15:22:16 -0300

    feat: perguntas novas

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,137 +1,149 @@
 import styled from "styled-components";
-import setaplay from "./assets/seta_play.png"
-import setavirar from "./assets/seta_virar.png"
-import acerto from "./assets/icone_certo.png"
-import erro from "./assets/icone_erro.png"
-import quase from "./assets/icone_quase.png"
+import setaplay from "./assets/seta_play.png";
+import setavirar from "./assets/seta_virar.png";
+import acerto from "./assets/icone_certo.png";
+import erro from "./assets/icone_erro.png";
+import quase from "./assets/icone_quase.png";
 
 export default function Flashcard(props) {
-    const { transformarCarta, setTransformarCarta } = props
-    const { respostaCarta, setRespostaCarta } = props
-    const { corVerde, setCorVerde, corVermelho, setCorVermelho, corAmarelo, setCorAmarelo, cor } = props
-    const { tracado, setTracado } = props
-    const { numeroPergunta } = props
-    let corr
+  const { transformarCarta, setTransformarCarta } = props;
+  const { respostaCarta, setRespostaCarta } = props;
+  const {
+    corVerde,
+    setCorVerde,
+    corVermelho,
+    setCorVermelho,
+    corAmarelo,
+    setCorAmarelo,
+    cor,
+  } = props;
+  const { tracado, setTracado } = props;
+  const { numeroPergunta } = props;
+  let corr;
 
-
-    function transformarcarta(index) {
-        if (cor != verde && cor != amarelo && cor != vermelho) {
-            console.log(index)
-            const novoArray = [...transformarCarta, index]
-            setTransformarCarta(novoArray)
-        }
-
+  function transformarcarta(index) {
+    if (cor != verde && cor != amarelo && cor != vermelho) {
+      const novoArray = [...transformarCarta, index];
+      setTransformarCarta(novoArray);
     }
-    function respostacarta(index) {
-        if (cor != verde && cor != amarelo && cor != vermelho) {
-            console.log(index)
-            const novoArray = [...respostaCarta, index]
-            setRespostaCarta(novoArray)
-            console.log(novoArray)
-        }
-
+  }
+  function respostacarta(index) {
+    if (cor != verde && cor != amarelo && cor != vermelho) {
+      const novoArray = [...respostaCarta, index];
+      setRespostaCarta(novoArray);
     }
-    function nada() {
-        console.log(nada)
-    }
+  }
+  function nada() {}
 
-    //assistir aula pensando como react
-    return (
-        <>
-            {
-                !transformarCarta.includes(numeroPergunta) ?
-                    (<PerguntaFechada 
-                        data-identifier="flashcard"
-                        cor=
-                        {corVerde.includes(numeroPergunta) ? verde :
-                            (corAmarelo.includes(numeroPergunta) ? amarelo :
-                                (corVermelho.includes(numeroPergunta) ? vermelho : ''))}
-                        tracado=
-                        {(corVerde.includes(numeroPergunta) ||
-                            corAmarelo.includes(numeroPergunta) ||
-                            corVermelho.includes(numeroPergunta)) ?
-                            tracado : ''}
-                        onClick=
-                        {(corVerde.includes(numeroPergunta) ||
-                            corAmarelo.includes(numeroPergunta) ||
-                            corVermelho.includes(numeroPergunta)) ?
-                            (nada) : (() => transformarcarta(numeroPergunta))}
-                    >
-                        <p>Pergunta {props.numeroPergunta + 1} </p>
-                        <img data-identifier="flashcard-status" src= 
-                        {corVerde.includes(numeroPergunta) ? acerto :
-                            (corAmarelo.includes(numeroPergunta) ? quase :
-                                (corVermelho.includes(numeroPergunta) ? erro : setaplay))} />
-                    </PerguntaFechada>
-                    ) : (!respostaCarta.includes(numeroPergunta) ?
-                        (
-                            <PerguntaAberta>
-                                <p>{props.pergunta} </p>
-                                <img src={setavirar} onClick={() => respostacarta(numeroPergunta)} />
-                            </PerguntaAberta>
-                        ) : (
-                            <PerguntaAberta>
-                                <p>{props.resposta} </p>
-                            </PerguntaAberta>
-                        )
-                    )
+  return (
+    <>
+      {!transformarCarta.includes(numeroPergunta) ? (
+        <PerguntaFechada
+          data-identifier="flashcard"
+          cor={
+            corVerde.includes(numeroPergunta)
+              ? verde
+              : corAmarelo.includes(numeroPergunta)
+              ? amarelo
+              : corVermelho.includes(numeroPergunta)
+              ? vermelho
+              : ""
+          }
+          tracado={
+            corVerde.includes(numeroPergunta) ||
+            corAmarelo.includes(numeroPergunta) ||
+            corVermelho.includes(numeroPergunta)
+              ? tracado
+              : ""
+          }
+          onClick={
+            corVerde.includes(numeroPergunta) ||
+            corAmarelo.includes(numeroPergunta) ||
+            corVermelho.includes(numeroPergunta)
+              ? nada
+              : () => transformarcarta(numeroPergunta)
+          }
+        >
+          <p>Pergunta {props.numeroPergunta + 1} </p>
+          <img
+            data-identifier="flashcard-status"
+            src={
+              corVerde.includes(numeroPergunta)
+                ? acerto
+                : corAmarelo.includes(numeroPergunta)
+                ? quase
+                : corVermelho.includes(numeroPergunta)
+                ? erro
+                : setaplay
             }
-        </>
-    );
+          />
+        </PerguntaFechada>
+      ) : !respostaCarta.includes(numeroPergunta) ? (
+        <PerguntaAberta>
+          <p>{props.pergunta} </p>
+          <img src={setavirar} onClick={() => respostacarta(numeroPergunta)} />
+        </PerguntaAberta>
+      ) : (
+        <PerguntaAberta>
+          <p>{props.resposta} </p>
+        </PerguntaAberta>
+      )}
+    </>
+  );
 }
 
-const verde = '#2FBE34'
-const amarelo = "#FF922E"
-const vermelho = "#FF3030"
+const verde = "#2FBE34";
+const amarelo = "#FF922E";
+const vermelho = "#FF3030";
 
 const PerguntaFechada = styled.div`
-    width: 300px;
-    height: 35px;
-    background-color: #FFFFFF;
-    margin: 12px;
-    padding: 15px;
-    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    color: ${props => props.cor};
-    text-decoration: ${props => props.tracado};
-    & :hover{
+  width: 300px;
+  height: 35px;
+  background-color: #ffffff;
+  margin: 12px;
+  padding: 15px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${(props) => props.cor};
+  text-decoration: ${(props) => props.tracado};
+  & :hover {
     cursor: pointer;
-}
-    p {
-        font-family: 'Recursive';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 16px;
-        line-height: 19px;
-    }
-`
-const PerguntaAberta = styled.div`
-    width: 300px;
-    margin: 12px;
-    padding: 15px;
-    min-height: 100px;
-    background: #FFFFD5;
-    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
-    border-radius: 5px;
-    font-family: 'Recursive';
+  }
+  p {
+    font-family: "Recursive";
     font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    line-height: 22px;
-    color: #333333;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    img{
-        position: absolute;
-        bottom: 10px;
-        right: 10px;
-    }& 
-    img:hover{
-        cursor: pointer ;
-    }
-`
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+  }
+`;
+const PerguntaAberta = styled.div`
+  width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #ffffd5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  img {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
+  & img:hover {
+    cursor: pointer;
+  }
+`;
--- src/Flashcards.js
+++ src/Flashcards.js
@@ -2,164 +2,167 @@ import { useState } from "react";
 import Flashcard from "./Flashcard";
 import styled from "styled-components";
 
-
 export default function Flashcards(props) {
-    const { transformarCarta, setTransformarCarta } = props
-    const { respostaCarta, setRespostaCarta } = props
-    const { corVerde, setCorVerde, corVermelho, setCorVermelho, corAmarelo, setCorAmarelo, cor } = props
-    const { tracado, setTracado } = props
-    const { numeroPergunta } = props
+  const { transformarCarta, setTransformarCarta } = props;
+  const { respostaCarta, setRespostaCarta } = props;
+  const {
+    corVerde,
+    setCorVerde,
+    corVermelho,
+    setCorVermelho,
+    corAmarelo,
+    setCorAmarelo,
+    cor,
+  } = props;
+  const { tracado, setTracado } = props;
+  const { numeroPergunta } = props;
 
-    return (
-        <>
-
-            <Flashcard
-                pergunta= {'Qual a melhor série do mundo?'}
-                resposta= {'Game of Thrones'}
-                numeroPergunta={numeroPergunta[0]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-            <Flashcard
-                pergunta= {'Qual o 9 do hexa?'}
-                resposta= {'Pedro'}
-                numeroPergunta={numeroPergunta[1]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-            <Flashcard
-                pergunta= {'Qual a protagonista de House of the dragon?'}
-                resposta= {'Rhaenyra'}
-                numeroPergunta={numeroPergunta[2]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-            <Flashcard
-                pergunta= {'Qual a melhor série da Netflix?'}
-                resposta= {'Dark'}
-                numeroPergunta={numeroPergunta[3]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-            <Flashcard
-                pergunta= {'Qual a melhor série do mundo?'}
-                resposta= {'Game of Thrones'}
-                numeroPergunta={numeroPergunta[4]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-            <Flashcard
-                pergunta= {'Qual a melhor série do mundo?'}
-                resposta= {'Game of Thrones'}
-                numeroPergunta={numeroPergunta[5]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-            <Flashcard
-                pergunta= {'Qual a melhor série do mundo?'}
-                resposta= {'Game of Thrones'}
-                numeroPergunta={numeroPergunta[6]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-            <Flashcard
-                pergunta= {'Qual a melhor série do mundo?'}
-                resposta= {'Game of Thrones'}
-                numeroPergunta={numeroPergunta[7]}
-                transformarCarta={transformarCarta}
-                setTransformarCarta={setTransformarCarta}
-                respostaCarta={respostaCarta}
-                setRespostaCarta={setRespostaCarta}
-                corVerde={corVerde}
-                setCorVerde={setCorVerde}
-                corAmarelo={corAmarelo}
-                setCorAmarelo={setCorAmarelo}
-                corVermelho={corVermelho}
-                setCorVermelho={setCorVermelho}
-                tracado={tracado}
-                setTracado={setTracado}
-                cor = {cor}
-            />
-
-        </>
-    );
+  return (
+    <>
+      <Flashcard
+        pergunta={"Qual a melhor série do mundo?"}
+        resposta={"Game of Thrones"}
+        numeroPergunta={numeroPergunta[0]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+      <Flashcard
+        pergunta={"Qual o 9 do hexa?"}
+        resposta={"Pedro"}
+        numeroPergunta={numeroPergunta[1]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+      <Flashcard
+        pergunta={"Qual a protagonista de House of the dragon?"}
+        resposta={"Rhaenyra"}
+        numeroPergunta={numeroPergunta[2]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+      <Flashcard
+        pergunta={"Qual a melhor série da Netflix?"}
+        resposta={"Dark"}
+        numeroPergunta={numeroPergunta[3]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+      <Flashcard
+        pergunta={"Quem é mais forte, Sanji ou Zoro?"}
+        resposta={"Sanji"}
+        numeroPergunta={numeroPergunta[4]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+      <Flashcard
+        pergunta={"Quem é o artilheiro brasileiro da libertadores?"}
+        resposta={"Gabigol"}
+        numeroPergunta={numeroPergunta[5]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+      <Flashcard
+        pergunta={"Qual a melhor transformação do goku?"}
+        resposta={"Super Sayajin 3"}
+        numeroPergunta={numeroPergunta[6]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+      <Flashcard
+        pergunta={"Qual a trindade da culinária piauiense?"}
+        resposta={"Maria Isabel, Paçoca e Creme de Galinha"}
+        numeroPergunta={numeroPergunta[7]}
+        transformarCarta={transformarCarta}
+        setTransformarCarta={setTransformarCarta}
+        respostaCarta={respostaCarta}
+        setRespostaCarta={setRespostaCarta}
+        corVerde={corVerde}
+        setCorVerde={setCorVerde}
+        corAmarelo={corAmarelo}
+        setCorAmarelo={setCorAmarelo}
+        corVermelho={corVermelho}
+        setCorVermelho={setCorVermelho}
+        tracado={tracado}
+        setTracado={setTracado}
+        cor={cor}
+      />
+    </>
+  );
 }
-
-
